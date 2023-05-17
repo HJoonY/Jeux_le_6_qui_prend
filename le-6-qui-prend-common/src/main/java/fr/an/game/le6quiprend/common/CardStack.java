6c13282a46b561521d5e553d5c78485cfc19903c
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2023-05-17 22:16:29 +0200

    add business rule on CardStack

--- le-6-qui-prend-common/src/main/java/fr/an/game/le6quiprend/common/CardStack.java
+++ le-6-qui-prend-common/src/main/java/fr/an/game/le6quiprend/common/CardStack.java
@@ -1,33 +1,74 @@
 package fr.an.game.le6quiprend.common;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * card stack, implementing game rules:
+ * <ul>
+ *     <li>when adding a card with value less than top value,
+ *     then take all the stack cards and put your card
+ *     </li>
+ *     <li>when adding a 6-th card,
+ *     then take all the stack cards (penalties)
+ *     and put your card that become the first of the stack.
+ *     </li>
+ *     <li>else when adding a card (at position 2,3,4,5),
+ *     then your are lucky, just put your card on the stack
+ *     </li>
+ * </ul>
+ */
 public class CardStack {
 
-    private List<Card> cards = new ArrayList<>();
+    private final List<Card> cards = new ArrayList<>(5);
+
+    @Getter // derived field, may also be recomputed each time
     private int sumPenalty;
+
+    @Getter // derived field, may also be recomputed each time
+    private int topValue;
 
     //---------------------------------------------------------------------------------------------
 
-    public int getSumPenalty() {
-        return sumPenalty;
+    public CardStack(Card firstCard) {
+        Objects.requireNonNull(firstCard);
+        resetWithTopCard(firstCard);
     }
+
+    //---------------------------------------------------------------------------------------------
 
     public int getCardCount() {
         return cards.size();
     }
 
-    public List<Card> addMayTakeIf6(Card c) {
+    public List<Card> addMayTakeIfBelowOr6th(Card c) {
+        Objects.requireNonNull(c);
         List<Card> res;
-        if (cards.size() == 5) {
+        if (c.value < topValue) {
             res = new ArrayList<>(cards);
             this.cards.clear();
+            resetWithTopCard(c);
         } else {
-            res = null;
+            if (cards.size() == 5) {
+                res = new ArrayList<>(cards);
+                this.cards.clear();
+                resetWithTopCard(c);
+            } else {
+                cards.add(c);
+                this.sumPenalty += c.penalty;
+                this.topValue = c.value;
+                res = null;
+            }
         }
-        cards.add(c);
         return res;
     }
 
+    protected void resetWithTopCard(Card card) {
+        this.cards.add(card);
+        this.sumPenalty = card.penalty;
+        this.topValue = card.value;
+    }
 }
